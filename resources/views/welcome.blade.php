b6c08e080e440a9b2546118963c003657fa2e008
rothbozz <Rothbozz77@gmail.com>
2024-01-20 13:50:30 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+   
+</head>
+
+<body class="antialiased">
+    <img height 10px width 20px src="https://external-preview.redd.it/9CYDK_raLy3xCnSFZPd0g4dt66EHJ8eY3wPV2QxzLKc.jpg?auto=webp&s=d0f63ec4e19c8ed087b600c5ead77ed8624d6238">
+    
+    <h1>Hello everyone</h1>
+</body>
+
+</html>
